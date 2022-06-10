1924082acad3be3c8fb689c6cfc40be8b30f9ac8
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-06-09 21:38:52 -0400

    Styled header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,32 @@
 import Head from "next/head";
 import { Button, Center, Flex, Text } from "@chakra-ui/react";
 
-export default function Header({ user, logout }) {
+export default function Header({ user, logout, isLoggingOut }) {
 	return (
 		<>
 			<header>
-				<Flex>
+				<Flex
+					px="10"
+					py="6"
+					justifyContent="space-between"
+					bg="teal.400"
+					color="white"
+				>
 					<Center>
-						<Text fontSize="5xl" fontWeight="bold" color="black">
+						<Text fontSize="xl" fontWeight="bold" color="black">
 							musicThoughts
 						</Text>
 					</Center>
 					<Center>
 						<Text>{user.getUsername()}</Text>
-						<Button onClick={logout}>Logout</Button>
+						<Button
+							ml="4"
+							colorScheme="teal"
+							onClick={logout}
+							disable={isLoggingOut}
+						>
+							Logout
+						</Button>
 					</Center>
 				</Flex>
 			</header>

194878d722d88a7fe99fedf57c736d8c175e1304
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-06-09 20:54:24 -0400

    Created basic login UI frame

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
+	const { isAuthenticated } = useMoralis();
+	if (!isAuthenticated) {
+		return (
+			<>
+				<Head>
+					<title>Login | musicThoughts</title>
+				</Head>
+				<Flex>
+					<Text>musicThoughts</Text>
+					<Button>Login with Metamask</Button>
+				</Flex>
+			</>
+		);
+	}
+
 	return <div>Hello world</div>;
 }

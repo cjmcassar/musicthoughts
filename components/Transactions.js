e7dc3e3fa56379ec801b453c7bb6308e3e9945c6
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-07-04 19:52:36 +0100

    Created transactions component

---
+++ components/Transactions.js
@@ -0,0 +1,47 @@
+import { Divider, Link, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { useMoralisWeb3Api } from "react-moralis";
+import CustomContainer from "./CustomContainer";
+
+function Transactions({ user }) {
+	const Web3Api = useMoralisWeb3Api();
+	const BASE_URL = "https://rinkeby.etherscan.io/tx/";
+
+	const [transactions, setTransactions] = useState([]);
+
+	const fetchTransactions = async () => {
+		const data = await Web3Api.account.getTransactions({
+			chain: "rinkeby",
+			address: user.get("ethAddress"),
+			limit: 5,
+		});
+		if (data) {
+			setTransactions(data.result);
+		}
+	};
+
+	useEffect(() => {
+		fetchTransactions();
+	}, []);
+
+	console.log(transactions);
+
+	return (
+		<CustomContainer>
+			<Text fontSize="xl" mb="6" fontWeight="bold">
+				My last 5 transactions
+			</Text>
+			{transactions &&
+				transactions.map((transaction) => (
+					<div key={transaction.hash}>
+						<Link href={`${BASE_URL}${transaction.hash}`} isExternal>
+							➡️&nbsp; {transaction.hash}
+						</Link>
+						<Divider />
+					</div>
+				))}
+		</CustomContainer>
+	);
+}
+
+export default Transactions;
